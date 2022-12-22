d41627e10fa88b74e3c1fd4c041dd8d024c1918a
kkmh90 <118958101+kkmh90@users.noreply.github.com>
2022-12-22 18:58:22 +0900

    자기소개 페이지에 내용 채우기

--- single_pages/views.py
+++ single_pages/views.py
@@ -3,13 +3,16 @@ from blog.models import Post
 
 def landing(request) :
     recent_posts = Post.objects.order_by('-pk')[:3]
-    return render(request, 'single_pages/landing.html')
+    return render(
+        request,
+        'single_pages/landing.html',
+        {
+            'recent_posts': recent_posts,
+        }
+    )
 
 def about_me(request) :
     return render(
         request,
         'single_pages/about_me.html',
-        {
-            'recent_posts': recent_posts,
-        }
     )
