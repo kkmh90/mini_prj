dab6f34f52fe825a1d766b02d695855a0e796ad2
kkmh90 <118958101+kkmh90@users.noreply.github.com>
2022-12-22 15:46:26 +0900

    사용자 아바타 보여주기

--- blog/models.py
+++ blog/models.py
@@ -74,6 +74,12 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/1403/853251dad316f0d6/svg/{self.author.email}'
+
     def __str__(self):
         return f'{self.author}::{self.content}'
 
